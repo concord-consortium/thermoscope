18b77c4929bc0bc8a4306334a556983dc357d35e
Chris Hart <chart@concord.org>
2016-12-05 10:20:03 -0500

    Minor change to sensor component

--- js/components/sensor.js
+++ js/components/sensor.js
@@ -8,23 +8,26 @@ export default class Sensor extends PureComponent {
   constructor(props) {
     super(props);
     this.state = { connected: false };
+    this.connectedSensor = this.props.sensor;
     this.handleIPAddressChange = this.handleIPAddressChange.bind(this);
     this.connect = this.connect.bind(this);
   }
+
   handleIPAddressChange(event){
     this.setState({ipAddress: event.target.value});
   }
 
   connect(event, value) {
     if (!this.state.connected) {
-      this.props.sensor.connect(this.state.ipAddress);
-      this.props.sensor.on('connected', this.connected.bind(this));
+      this.connectedSensor.connect(this.state.ipAddress);
+      this.connectedSensor.on('connected', this.connected.bind(this));
     }
   }
 
   connected(event) {
     this.setState({ connected: true });
   }
+
   render() {
     let connectStatus = this.state.connected ? "Connected." : "";
     return (
