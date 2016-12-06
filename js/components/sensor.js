45c88c905fa77bcf3e1b7eae5635e9bcc3c5ed53
Chris Hart <chart@concord.org>
2016-12-06 14:54:11 -0500

    Additional styling for the sensor connection UI

--- js/components/sensor.js
+++ js/components/sensor.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import TextField from 'material-ui/TextField';
+import LinearProgress from 'material-ui/LinearProgress';
 import RaisedButton from 'material-ui/RaisedButton';
 
 import '../../css/sensor-connect.less';
@@ -7,10 +8,11 @@ import '../../css/sensor-connect.less';
 export default class Sensor extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = { connected: false };
+    this.state = { connected: false , connecting: false, showDetails: false};
     this.connectedSensor = this.props.sensor;
     this.handleIPAddressChange = this.handleIPAddressChange.bind(this);
     this.connect = this.connect.bind(this);
+    this.toggleDisplay = this.toggleDisplay.bind(this);
   }
 
   handleIPAddressChange(event){
@@ -19,22 +21,35 @@ export default class Sensor extends PureComponent {
 
   connect(event, value) {
     if (!this.state.connected) {
+      this.setState({ connecting: true });
       this.connectedSensor.connect(this.state.ipAddress);
       this.connectedSensor.on('connected', this.connected.bind(this));
     }
   }
 
   connected(event) {
-    this.setState({ connected: true });
+    this.setState({ connected: true, connecting: false });
+  }
+
+  toggleDisplay(event) {
+    this.setState({ showDetails: this.state.showDetails ? false : true});
   }
 
   render() {
-    let connectStatus = this.state.connected ? "Connected." : "";
+    const { connected, connecting, showDetails } = this.state;
+    let connectStatus = connected ? "Connected." : "";
     return (
-      <div className="sensorConnect">
-        <TextField hintText="IP Address" ref="ip" type="text" id="ipAddress" onChange={this.handleIPAddressChange}></TextField>
-        <RaisedButton id="connect" onClick={this.connect}>Connect</RaisedButton>
-        <div id="sensorConnectionStatus">{connectStatus}</div>
+        <div className="sensorConnect">
+          <div id="toggleSensorDisplay" onClick={this.toggleDisplay}><i className="material-icons">settings_input_antenna</i></div>
+            {showDetails &&
+                <div className="sensorDetails">
+                  <TextField hintText="IP Address" ref="ip" type="text" id="ipAddress" onChange={this.handleIPAddressChange}></TextField>
+                  <RaisedButton id="connect" onClick={this.connect}>Connect</RaisedButton>
+                  {connecting &&
+                    <LinearProgress />
+                  }
+                  {!connecting && <div id="sensorConnectionStatus">{connectStatus}</div>}
+                </div>}
       </div>
     )
   }
