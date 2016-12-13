c983e2c947682206603f9155912210fd74e26b34
Sam Fentress <sfentress@concord.org>
2016-12-13 14:58:11 -0500

    Add sliders for numerical values

--- js/particle-modeler/authoring.js
+++ js/particle-modeler/authoring.js
@@ -1,45 +1,54 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Slider from 'material-ui/Slider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Required by Material-UI library.
+injectTapEventPlugin();
 
 const Authoring = (props) => {
-  let handleChange = function(evt) {
-    props.onChange(evt.target.dataset.prop, evt.target.value);
-  }
-
   let handleCheckboxChange = function (evt) {
     props.onChange(evt.target.dataset.prop, evt.target.checked);
   }
 
   let createCheckboxInput = function(property, label) {
     return (
-      <p>
-        <label>
-          {label}:
-          <input type="checkbox" data-prop={property} checked={props[property]} onChange={handleCheckboxChange} />
-        </label>
-      </p>
+      <div>
+        {label}:
+        <input type="checkbox" data-prop={property} checked={props[property]} onChange={handleCheckboxChange} />
+      </div>
     );
   }
 
-  let createTextInput = function(property, label) {
+  let createSliderInput = function(property, label) {
+    let handleChange = function(evt, val) {
+      props.onChange(property, val);
+    }
     return (
-      <p>
-        <label>
-          {label}:
-          <input type="text" data-prop={property} value={props[property]} onChange={handleChange} />
-        </label>
-      </p>
+      <div>
+        {label}:
+        <Slider
+          min={0}
+          max={100}
+          value={props[property]}
+          onChange={handleChange}
+          sliderStyle={{ marginTop: 5, marginBottom: 5 }}
+        />
+      </div>
     );
   }
 
   return (
-    <div className="authoring-form">
-      <h3>Authoring</h3>
-      { createCheckboxInput("temperatureControl", "Heatbath") }
-      { createTextInput("targetTemperature", "Heatbath Temperature") }
-      { createTextInput("gravitationalField", "Gravity") }
-      { createTextInput("timeStep", "Time Step") }
-      { createTextInput("viscosity", "Viscosity") }
-    </div>
+    <MuiThemeProvider>
+      <div className="authoring-form">
+        <h3>Authoring</h3>
+        { createCheckboxInput("temperatureControl", "Heatbath") }
+        { createSliderInput("targetTemperature", "Heatbath Temperature") }
+        { createSliderInput("gravitationalField", "Gravity") }
+        { createSliderInput("timeStep", "Time Step") }
+        { createSliderInput("viscosity", "Viscosity") }
+      </div>
+    </MuiThemeProvider>
   )
 }
 
