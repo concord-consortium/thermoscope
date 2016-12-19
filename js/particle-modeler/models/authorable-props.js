eac89c2ae3efcf178360358dd3c19adc2804d1e7
Sam Fentress <sfentress@concord.org>
2016-12-19 16:10:18 -0500

    Move authorable props to own file

---
+++ js/particle-modeler/models/authorable-props.js
@@ -0,0 +1,39 @@
+module.exports = {
+  authoring: false,
+  temperatureControl: {
+    label: "Heatbath",
+    value: false
+  },
+  targetTemperature: {
+    label: "Heatbath temperature",
+    value: 0,
+    min: 0,
+    max: 1000
+  },
+  gravitationalField: {
+    label: "Gravity",
+    value: 0,
+    min: 0,
+    max: 1e-5
+  },
+  timeStep: {
+    label: "Time step",
+    value: 1,
+    min: 0,
+    max: 5
+  },
+  viscosity: {
+    label: "Viscosity",
+    value: 1,
+    min: 0,
+    max: 10
+  },
+  showFreezeButton: {
+    label: "Show Freeze Button",
+    value: false
+  },
+  startWithAtoms: {
+    label: "Start With Existing Atoms",
+    value: false
+  }
+}
