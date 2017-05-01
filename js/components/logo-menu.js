eca70f3aa565de899d770b4ad653e28e8a322a95
Chris Hart <chart@concord.org>
2017-05-01 10:29:09 -0400

    Embed small branding images as resources to fix file path issues

--- js/components/logo-menu.js
+++ js/components/logo-menu.js
@@ -10,13 +10,14 @@ export default class LogoMenu extends PureComponent {
 
     return (
       <div className={scale}>
+        <div className="cc-logo" />
         {showNav &&
           <div className="menu">
             <div title="Home"><a href="../"><i className="material-icons">home</i></a></div>
             <div title="Thermoscope"><a href="../thermoscope"><i className="material-icons">group_work</i></a></div>
             <div title="Particle Modeler"><a href="../particle-modeler"><i className="material-icons">all_out</i></a></div>
           </div>}
-        <img className="cc-logo" src="../css/concord-consortium-logo.png" />
+
       </div>
     )
   };
