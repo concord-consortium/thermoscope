d803a00832e0d578f0302ff73dec18c4451ac2ed
Chris Hart <chart@concord.org>
2017-05-26 10:29:16 -0400

    Store current username in a cookie for 1 day for repeated sessions

--- js/components/user-name-generator.js
+++ js/components/user-name-generator.js
@@ -68,9 +68,45 @@ let getRandomInt = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-module.exports = function () {
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+
+function generateUsername() {
   let color = colors[getRandomInt(0, colors.length - 1)];
   let animal = animals[getRandomInt(0, animals.length - 1)];
   let num = getRandomInt(100, 999);
   return color + animal + num;
+}
+
+module.exports = function(){
+  let username = getCookie("username");
+  if (username == "") {
+    username = generateUsername();
+    if (username != "" && username != null) {
+      setCookie("username", username, 1);
+    }
+  }
+  return username;
 }
