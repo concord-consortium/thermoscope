24f21b641a9c5ad3f15763d54d953d418ed23c99
Chris Hart <chart@concord.org>
2017-10-26 21:40:29 -0400

    bump version number on Master branch

--- js/components/landing-page.js
+++ js/components/landing-page.js
@@ -67,7 +67,7 @@ export default class LandingPage extends PureComponent {
           </div>
         </div>
           }
-        <div className="version-identifier">Master 20171024.1</div>
+        <div className="version-identifier">Master 20171026.1</div>
       </div>
     </MuiThemeProvider>
     );
