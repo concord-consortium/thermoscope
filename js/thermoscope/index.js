4849c61916af5ebd8058dffb3344b0d0fc4c3e38
Colin Greenhill <clgreenhill@gmail.com>
2017-11-15 14:02:16 -0500

    Added icons to thermoscope home page options [#152694241]

--- js/thermoscope/index.js
+++ js/thermoscope/index.js
@@ -10,6 +10,7 @@ import bleSensor from '../components/ble-sensor.js';
 import LogoMenu from '../components/logo-menu';
 import { getURLParam } from '../utils';
 import { List, ListItem } from 'material-ui/List';
+import { GridList, GridTile } from 'material-ui/GridList'
 import Thermoscope from '../components/thermoscope';
 
 const sensor = bleSensor;
@@ -94,6 +95,11 @@ export default class ThermoscopeControl extends PureComponent {
 
   render() {
     const { mode } = this.state;
+    const gridStyle = {
+      display: 'flex',
+      flexWrap: 'nowrap',
+      overflowX: 'auto',
+    }
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         <div className="app">
@@ -101,19 +107,40 @@ export default class ThermoscopeControl extends PureComponent {
           <div title="Home" className="main-menu-button" onClick={this.showMenu} ><i className="material-icons">home</i></div>
           { mode === ThermoscopeMode.Menu &&
             <div className="demo-links">
-            <List>
-            <div className="list-section">
+              <div className="list-section">
                 <h1>Thermoscope Examples</h1>
-              <ListItem primaryText="Thermoscope (solid) Wood and Stone" onClick={() => this.setThermoscopeRendering("A=solid&B=solid", 2)} key="1" />
-              <ListItem primaryText="Thermoscope (liquid) Oil and Soap" onClick={() => this.setThermoscopeRendering("A=liquid&B=liquid", 2)} key="2" />
-              <ListItem primaryText="Thermoscope (gas) Air" onClick={() => this.setThermoscopeRendering("A=gas&B=gas", 2)} key="3" />
+                <GridList style={gridStyle}>
+                  <GridTile onClick={() => this.setThermoscopeRendering("A=solid&B=solid", 2)} key="1">
+                    <img src="../images/blocks-100px.png"/>
+                    <img src="../images/quartz-100px.png"/>
+                    <div>Wood and Stone</div>
+                  </GridTile>
+                  <GridTile onClick={() => this.setThermoscopeRendering("A=liquid&B=liquid", 2)} key="2" >
+                    <img src="../images/oil-100px.png"/>
+                    <img src="../images/soap-100px.png"/>
+                    <div>Oil and Soap</div>
+                  </GridTile>
+                  <GridTile onClick={() => this.setThermoscopeRendering("A=gas&B=gas", 2)} key="3" >
+                    <img src="../images/air-100px.png"/>  
+                    <img src="../images/air-100px.png"/>
+                    <div>Air</div>
+                  </GridTile>
+                </GridList>
               </div>
               <div className="list-section">
                 <h1>Configurable Thermoscopes</h1>
-              <ListItem primaryText="Thermoscope (one)" onClick={() => this.setThermoscopeRendering("controls=true", 1)} key="4" />
-              <ListItem primaryText="Thermoscope (two)" onClick={() => this.setThermoscopeRendering("controls=true", 2)} key="5" />
-               </div>
-               </List>
+                <GridList style={gridStyle}>
+                  <GridTile onClick={() => this.setThermoscopeRendering("controls=true", 1)} key="4">
+                    <img src="../css/thermoscope_icon_84.png"/>
+                    <div>Thermoscope (one)</div> 
+                  </GridTile>
+                  <GridTile onClick={() => this.setThermoscopeRendering("controls=true", 2)} key="5">
+                    <img src="../css/thermoscope_icon_84.png"/>
+                    <img src="../css/thermoscope_icon_84.png"/>
+                    <div>Thermoscope (two)</div>
+                  </GridTile>
+                </GridList>
+              </div>
             </div>
           }
           {mode === ThermoscopeMode.OneThermoscope &&
