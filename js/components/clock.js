f7da8d460c47c6316e8bbff20e0ba47b60054264
Chris Hart <chart@concord.org>
2018-04-23 15:51:27 -0400

    Added a small blue circular loading indicator bottom-right of screen when cache is downloading [#156618679]

--- js/components/clock.js
+++ js/components/clock.js
@@ -1,4 +1,5 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
 import '../../css/app.less';
 
 export default class Clock extends PureComponent {
@@ -19,16 +20,39 @@ export default class Clock extends PureComponent {
   }
 
   tick() {
+    let loading = false,
+    appCache = window.applicationCache;
+    switch (appCache.status) {
+      case appCache.CHECKING: // CHECKING == 2
+        loading = true;
+        break;
+      case appCache.DOWNLOADING: // DOWNLOADING == 3
+        loading = true;
+        break;
+      default:
+        loading = false;
+        break;
+    }
     this.setState({
-      date: new Date()
+      date: new Date(),
+      loading
     });
   }
 
   render() {
+    const { loading, date } = this.state;
     let clockClassName = this.props.dark ? 'clock-dark' : 'clock';
+    const width = 20;
     return (
-      <div className={clockClassName}>
-        <span>{this.state.date.toLocaleTimeString()}</span>
+      <div className='timed-elements'>
+        <div className={clockClassName}>
+          <span>{date.toLocaleTimeString()}</span>
+        </div>
+        {loading &&
+          <div className='cache-progress-container'>
+            <CircularProgress className='cache-progress' size={width} thickness={5} style={{position: 'fixed', bottom: width * 0.5, right: width * 0.5}}/>
+          </div>
+        }
       </div>
     )
   };
