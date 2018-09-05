a81ecd1b82db536161a6f9c0beff226e2aa9a338
Sam Fentress <sfentress@concord.org>
2018-09-05 11:22:45 -0400

    Build files for PhoneGap

--- js/particle-modeler/models/index.js
+++
@@ -1,9 +0,0 @@
-import interactive from './interactive.json';
-import baseModel from './model.json';
-import emptyModel from './empty-model.json';
-
-export default {
-  interactive,
-  baseModel,
-  emptyModel
-}
