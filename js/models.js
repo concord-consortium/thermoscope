999dca903c1510c9a39ccd9bca43b154f52b5e35
Chris Hart <chart@concord.org>
2019-06-12 12:16:14 -0400

    Make models available for selection

--- js/models.js
+++ js/models.js
@@ -10,6 +10,8 @@ import liquid2 from '../models/liquid-2.json';
 import liquid3 from '../models/liquid-3.json';
 import liquid4 from '../models/liquid-4.json';
 import mixing from '../models/mixing.json';
+import mixinga from '../models/mixing-a.json';
+import mixingb from '../models/mixing-b.json';
 import gas1 from '../models/gas-1.json';
 import gas3 from '../models/gas-3.json';
 import gas4 from '../models/gas-4.json';
@@ -81,6 +83,26 @@ export default {
       timeStepScale: function (temp) {
         return normalizeTemp(temp) * 0.55 + 0.25;
       }
+    },
+    {
+      name: 'MixingA',
+      json: mixinga,
+      tempScale: function (temp) {
+        return normalizeTemp(temp) * 1500 + 500;
+      },
+      timeStepScale: function (temp) {
+        return normalizeTemp(temp) * 0.55 + 0.25;
+      }
+    },
+    {
+      name: 'MixingB',
+      json: mixingb,
+      tempScale: function (temp) {
+        return normalizeTemp(temp) * 1500 + 500;
+      },
+      timeStepScale: function (temp) {
+        return normalizeTemp(temp) * 0.55 + 0.25;
+      }
     }
   ],
   gas: [
@@ -124,7 +146,6 @@ export default {
         if (t < 0.65)
           return 1e-6
         else {
-          console.log("wee!")
           return 3e-7
         }
       },
